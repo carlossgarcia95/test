67d67a94997ca33bb937ac47926c15d427a00e5f
Carlos Garcia <carlosgarcia3995@gmail.com>
2025-02-20 13:34:11 -0800

    Second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,72 @@
-import { Badge } from "@/components/ui/badge";
+import * as React from "react";
+
 import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Badge } from "@/components/ui/badge";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <Select defaultValue="blueberry">
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select a fruit" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectGroup>
-            <SelectLabel>Fruits</SelectLabel>
-            <SelectItem value="apple">
-              <Badge variant="outline">Apple</Badge>
-            </SelectItem>
-            <SelectItem value="banana">
-              <Badge variant="secondary">Banana</Badge>
-            </SelectItem>
-            <SelectItem value="blueberry">
-              <Badge>Blueberry</Badge>
-            </SelectItem>
-            <SelectItem value="grapes">
-              <Badge>Grapes</Badge>
-            </SelectItem>
-            <SelectItem value="pineapple">
-              <Badge>Pineapple</Badge>
-            </SelectItem>
-          </SelectGroup>
-        </SelectContent>
-      </Select>
-      <Button>Hello world</Button>
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="framework">Framework</Label>
+                <Select defaultValue="next">
+                  <SelectTrigger id="framework">
+                    <SelectValue placeholder="Select" />
+                  </SelectTrigger>
+                  <SelectContent position="popper">
+                    <SelectItem value="next">
+                      <Badge>Next.js</Badge>
+                    </SelectItem>
+                    <SelectItem value="react">
+                      <Badge>React</Badge>
+                    </SelectItem>
+                    <SelectItem value="redux">
+                      <Badge>Redux</Badge>
+                    </SelectItem>
+                    <SelectItem value="python">
+                      <Badge>Python</Badge>
+                    </SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
